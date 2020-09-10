333ab0439aa5f13b19b5046b64da668a3078762c
Arcangel Artigue <artigue_arcangel@hotmail.com>
2020-09-10 20:34:15 -0300

    feat: first commit

---
+++ src/main/java/com/example/graphqlspringbootclient/GraphqlSpringbootClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.graphqlspringbootclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlSpringbootClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlSpringbootClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/graphqlspringbootclient/GraphqlSpringbootClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.graphqlspringbootclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlSpringbootClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
